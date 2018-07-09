502a3a224811f4606a4bb6fd333081eb9c227b0e
Student <student@Admins-MacBook-Air-5.local>
2018-07-09 12:00:30 -0700

    update

--- hello.js
+++ hello.js
@@ -1,1 +1,9 @@
-// Author: FirstName LastName
+// Author: Antonio Farias
+
+console.log("hello fellow lizard humanoids" );
+console.log("Hello World!");
+console.log("It's a beautiful day for programming, isn't it?");
+console.log("Hey! What's 1 + 1?");
+console.log("It's " + 1 + 1 + " of course!");
+console.log("No, it's " + (1 + 1) + ", silly goose!");
+// console.log("This isn't going to get printed, is it?");
